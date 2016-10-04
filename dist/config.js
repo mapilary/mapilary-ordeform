e8765e91fff404f4721675397a09db3f61c53b15
Marian Hello <marian.hello@gmail.com>
2016-10-04 10:58:33 +0200

    allow running from rawgithub

--- dist/config.js
+++ dist/config.js
@@ -9,21 +9,21 @@
           baseUrl = 'http://localhost:4444/';
           widgetUrl = 'http://localhost:8000/mapilary-widget-ng/build/';
           break;
-        case 'demo.mapilary.com':
-          baseUrl = 'https://api.mapilary.com/test/';
-          widgetUrl = 'http://demo.mapilary.com/widget/';
-          break;
         case 'dev.mapilary.com':
           baseUrl = 'http://api2.mapilary.com/dev/';
           widgetUrl = 'http://dev.mapilary.com/widget/';
+          break;
+        case 'mapilary.com':
+          baseUrl = 'https://api.mapilary.com/test/';
+          widgetUrl = 'http://mapilary.com/widget/';
           break;
         case 'test.mapilary.com':
           baseUrl = 'https://api.mapilary.com/test/';
           widgetUrl = 'http://demo.mapilary.com/widget/';
           break;
-        case 'xpressgo.com':
-          baseUrl = 'https://api.xpressgo.com/v2/';
-          widgetUrl = 'http://xpressgo.com/widget/';
+        default:
+          baseUrl = 'http://api2.mapilary.com/dev/';
+          widgetUrl = 'http://dev.mapilary.com/widget/';
           break;
     }
 
